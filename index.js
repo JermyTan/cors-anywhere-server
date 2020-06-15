55ef40dc44228de86c9c67896827f5990be1e6f3
Jeremy <tan_kai_qun97@yahoo.com.sg>
2020-06-15 21:04:30 +0800

    Add cors anywhere server

---
+++ index.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+
+const host = process.env.HOST ?? "localhost";
+const port = process.env.PORT ?? "4000";
+
+function parseEnvList(envVariable) {
+  return envVariable?.split(",") ?? [];
+}
+
+const originWhitelist = parseEnvList(process.env.CORSANYWHERE_WHITELIST);
+
+const corsProxy = require("cors-anywhere");
+corsProxy
+  .createServer({
+    originWhitelist,
+    requireHeader: ["origin", "x-requested-with"],
+    removeHeaders: [
+      "cookie",
+      "cookie2",
+      // Strip Heroku-specific headers
+      "x-heroku-queue-wait-time",
+      "x-heroku-queue-depth",
+      "x-heroku-dynos-in-use",
+      "x-request-start",
+    ],
+    redirectSameOrigin: true,
+    httpProxyOptions: {
+      // Do not add X-Forwarded-For, etc. headers, because Heroku already adds it.
+      xfwd: false,
+    },
+  })
+  .listen(port, host, () =>
+    console.log(`Jeremy's CORS Anywhere server is running on ${host}:${port}`)
+  );
